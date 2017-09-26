4bac6b84c143c8c1d5a6545d845a188db527d5eb
wangyu48 <wangyu48@lenovo.com>
2017-09-26 18:55:09 +0800

    haha

---
+++ src/java/com/wy/web/controller/UserController.java
@@ -0,0 +1,35 @@
+package com.wy.web.controller;
+
+import com.wy.domain.UserT;
+import com.wy.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+
+    @Autowired
+    private UserService userService;
+    @RequestMapping("/get")
+    public String getUser(Model model){
+        System.out.println("user");
+        UserT user = userService.getUser();
+        System.out.println(user.getUserName());
+        model.addAttribute("user",user);
+        return "index";
+    }
+
+1
+
+}
+
+
+
+
